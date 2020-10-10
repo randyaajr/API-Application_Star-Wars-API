3be6a70d3a97659d0e8184ae038e9b5d3847a9a6
randyaajr <randyaajrsp@gmail.com>
2020-10-10 00:41:42 +0000

    Created a new function so that i could see my data within the browser console

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,20 +1,17 @@
 var xhr = new XMLHttpRequest(); 
-
 var data;
-                                
+                
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); 
-                                                        
-xhr.send();                                             
+xhr.send();    
 
-
+function setData(jsonData){
+    data =jsonData;
+    console.log(data);
+}
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
+        setData(JSON.parse(this.responseText));
      }
-};       
-        
-                                                        
-console.log(data);
-                                                       
+}; 
  
