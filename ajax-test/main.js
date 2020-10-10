adc6e97e6ffa35fd79023b121bc00fc09379f4ef
randyaajr <randyaajrsp@gmail.com>
2020-10-10 00:51:04 +0000

    Undefined 234 in console. Used timeout function to fix this

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -2,16 +2,13 @@ var xhr = new XMLHttpRequest();
 var data;
                 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); 
-xhr.send();    
-
-function setData(jsonData){
-    data =jsonData;
-    console.log(data);
-}
+xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
      }
 }; 
- 
+ setTimeout(function() {
+     console.log(data);
+ }, 500);
