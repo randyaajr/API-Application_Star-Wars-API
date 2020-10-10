23a666249f39a738dd538a9c0d13dd321025cf53
randyaajr <randyaajrsp@gmail.com>
2020-10-10 01:08:34 +0000

    called my getData function, passed in the 'printDataToConsole' function as an argument.yay callback function worked!

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,14 +1,18 @@
-var xhr = new XMLHttpRequest(); 
-var data;
-                
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/"); 
-xhr.send();
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
-     }
-}; 
- setTimeout(function() {
-     console.log(data);
- }, 500);
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
