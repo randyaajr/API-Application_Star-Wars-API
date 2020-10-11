9061064356a10f4ce5babe9979da43a24a0c7913
randyaajr <randyaajrsp@gmail.com>
2020-10-11 21:55:24 +0000

    Forgot the trailing '/' after the type on line 5

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -2,7 +2,7 @@ const baseURL = "https://ci-swapi.herokuapp.com/api/";
 function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type);
+    xhr.open("GET", baseURL + type + "/");
     xhr.send();
 
     xhr.onreadystatechange = function() {
